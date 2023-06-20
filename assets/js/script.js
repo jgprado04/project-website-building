77ea2c626a6a745d484fd12feff6154270a5223f
JG <jgprado04@gmail.com>
2023-06-20 16:32:29 -0300

    Botão menu - feito / Menu - feito / Responsividade contact - ok (Falta olhar os textos, mas praticamente a responsivdade está incrível)

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function doMenu() {
+    let menuArea = document.querySelector(".area-menu")
+    if(menuArea.style.display == "none")  {
+        menuArea.style.display = "flex";
+    } else {
+        menuArea.style.display = "none";
+    }
+}
+
+document.querySelector(".open-menu").addEventListener("click", doMenu);
