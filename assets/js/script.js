a8b19ce9f1c6b23176cfa2086940124c7f579aa0
JG <jgprado04@gmail.com>
2023-06-23 18:03:02 -0300

    Ajeitei todas as ordens alfabéticas e fiz o sistema de pull down do menu só falta do menu responsivo

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,27 @@ function doMenu() {
 }
 
 document.querySelector(".open-menu").addEventListener("click", doMenu);
+
+function doAbout() {
+    window.scrollTo(
+        {
+            top: 2600,
+            left: 0,
+            behavior: 'smooth'
+        }
+    )
+}
+
+document.querySelector(".about-our").addEventListener("click", doAbout);
+
+function doContact() {
+    window.scrollTo(
+        {
+            top: 3100,
+            left: 0,
+            behavior: 'smooth'
+        }
+    )
+}
+
+document.querySelector(".contact").addEventListener("click", doContact);
