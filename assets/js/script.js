4f0484d1f34fbfaf371debb09da78f1ae1ca6919
JG <jgprado04@gmail.com>
2023-07-02 16:01:22 -0300

    Website completo, revi os códigos, tudo ok (Falta ainda o pdf da empresa)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 // Para abrir  o menu
 document.querySelector("#open-menu").addEventListener("click", () => {
     let menuArea = document.querySelector(".area-menu")
-    if (menuArea.style.display == "none") {
-        menuArea.style.display = "flex";
+    if (menuArea.style.height == "0px") {
+        menuArea.style.height = "200px";
     } else {
-        menuArea.style.display = "none";
+        menuArea.style.height = "0px";
     }
 });
 
@@ -13,7 +13,7 @@ document.querySelector("#open-menu").addEventListener("click", () => {
 document.querySelector("#contact").addEventListener("click", () => {
     window.scrollTo(
         {
-            top: 3500,
+            top: 6000,
             left: 0,
             behavior: 'smooth'
         }
@@ -31,3 +31,22 @@ document.querySelector("#contact-menu").addEventListener("click", () => {
         }
     )
 });
+
+window.addEventListener("scroll", () => {
+    let arrowArea = document.querySelector(".arrow-up-area");
+    if (window.scrollY >= 1000) {
+        arrowArea.style.display = "block";
+    } else {
+        arrowArea.style.display = "none";
+    }
+})
+
+document.querySelector("#arrow-up").addEventListener("click", () => {
+    window.scrollTo(
+        {
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        }
+    )
+})
